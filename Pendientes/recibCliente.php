c8d25a31938a0a810531dcd5d1e75f29e488a5c7
=Jeferson Vargas <=jeferson.vargas@inacapmail.cl>
2024-01-28 03:17:58 -0300

    Se modifico al crear un nuevo pendiente este verificado

--- Pendientes/recibCliente.php
+++ Pendientes/recibCliente.php
@@ -5,79 +5,64 @@
 
 <?php
 include 'bd.php';
+
 $hora_actual = date('H:i:s');
 
-$dato1      = $_REQUEST['fila1'];
-$dato2      = $_REQUEST['fila2'];
-$dato3      = $_REQUEST['fila3'];
-$dato4      = $_REQUEST['fila4'];
-$dato6      = $_REQUEST['fila6'];
-$dato8      = $_REQUEST['fila8'];
+// Obtener datos del formulario
+$dato1        = $_REQUEST['fila1'];
+$dato2        = $_REQUEST['fila2'];
+$dato3        = $_REQUEST['fila3'];
+$dato4        = $_REQUEST['fila4'];
+$dato6        = $_REQUEST['fila6'];
+$dato8        = $_REQUEST['fila8'];
 $fecha_nueva  = $_REQUEST['fila10'];
 $fecha_ultima = $_REQUEST['fila11'];
-$link       = $_REQUEST['link'];
+$link         = $_REQUEST['link'];
 
-if (isset($_REQUEST["Anime"])) {
-    $checkbox    = $_REQUEST['Anime'];
-    // Hacer algo con $checkbox1Value
-    echo "Anime_Verdadero<br>";
-    echo $checkbox . "<br>";
-} else {
-    $checkbox = "NO";
-    echo "Anime_Falso<br>";
-    echo $checkbox . "<br>";
-}
+// Verificar si la casilla "Anime" está marcada
+$checkbox = isset($_REQUEST["Anime"]) ? $_REQUEST['Anime'] : "NO";
+echo ($checkbox ? "Anime_Verdadero" : "Anime_Falso") . "<br>";
 
 $fecha_now = date('Y-m-d H:i:s', strtotime($fecha_nueva . ' ' . $hora_actual));
 
-$sql      = ("SELECT * FROM $tabla where $fila1='$dato1';");
+// Consulta para verificar si existe un registro con el mismo dato1
+$sql      = "SELECT * FROM $tabla WHERE $fila1='$dato1'";
 $consulta = mysqli_query($conexion, $sql);
 
-echo $sql;
-echo "<br>";
-echo $fila1;
-echo "<br>";
-echo $dato2;
-echo "<br>";
-echo "Fecha Ultimo Capitulo : " . $fecha_ultima;
-echo "<br>";
-echo "Fecha Nuevo Capitulo : " . $fecha_nueva;
-echo "<br>";
+// Mostrar algunos resultados y detalles
+echo $sql . "<br>";
+echo "$fila1: $dato1<br>";
+echo "Fecha Ultimo Capitulo: $fecha_ultima<br>";
+echo "Fecha Nuevo Capitulo: $fecha_nueva<br>";
+
+// Calcular la diferencia de fechas
+$fechaInicio = new DateTime($fecha_nueva);
+$fechaFin    = new DateTime($fecha_ultima);
+$diferencia  = $fechaInicio->diff($fechaFin);
+$dias        = $diferencia->days;
+echo "Días de diferencia: $dias<br>";
+
+// Determinar el estado
+$estado = ($dato2 == "") ? "Faltante" : "Correcto";
+echo "Estado: $estado<br>";
+
 $Tabla = ucfirst($tabla);
 
-$fechaInicio = new DateTime($fecha_nueva);
-$fechaFin = new DateTime($fecha_ultima);
-$diferencia = $fechaInicio->diff($fechaFin);
-
-$dias = $diferencia->days;
-echo $dias;
-echo "<br>";
-
-if ($dato2 == "") {
-    $estado = "Faltante";
-} else {
-    $estado = "Correcto";
-}
-echo $estado;
-echo "<br>";
-
-
 if (mysqli_num_rows($consulta) == 0) {
+    // Registro no existe, realizar inserción y otras operaciones
 
-    echo "$dato1 no existe en $tabla";
-    echo "<br>";
-
+    echo "$dato1 no existe en $tabla<br>";
 
     try {
-        $sql = "INSERT INTO $tabla(`$fila1`,`$fila2`,`$fila3`, `$fila4`, `$fila6`,`$fila8`,`$fila10`,`$fila11`,`$fila13`,`$ver`,`Anime`,`$fila17`) VALUES( '" . $dato1 . "','" . $dato2 . "','" . $dato3 . "','" . $dato4 . "','" . $dato6 . "','" . $dato8 . "','" . $fecha_nueva . "','" . $fecha_ultima . "','" . $estado . "','NO','" . $checkbox . "',NOW())";
+        $sql = "INSERT INTO $tabla(`$fila1`,`$fila2`,`$fila3`, `$fila4`, `$fila6`,`$fila8`,`$fila10`,`$fila11`,`$fila13`,`$ver`,`Anime`,`$fila17`) 
+                VALUES('$dato1', '$dato2', '$dato3', '$dato4', '$dato6', '$dato8', '$fecha_nueva', '$fecha_ultima', '$estado', 'SI', '$checkbox', NOW())";
+
         $resultado = mysqli_query($conexion, $sql);
-        echo $sql;
-        echo "<br>";
+        echo $sql . "<br>";
     } catch (PDOException $e) {
         $conn = null;
-        echo $sql;
-        echo "<br>";
-        echo $e;
+        echo $sql . "<br>";
+        echo $e . "<br>";
     }
 
     echo '<script>
@@ -85,83 +70,71 @@ if (mysqli_num_rows($consulta) == 0) {
             icon: "success",
             title: "Creando Registro de ' . $dato1 . '  en  ' . $titulo7 . '",
             confirmButtonText: "OK"
-        }).then(function() {
+        }).then(() => {
             window.location = "' . $link . '"; 
         });
         </script>';
 
-    $consulta1 = "SELECT * FROM `$tabla` where Nombre='$dato1'";
+    $consulta1 = "SELECT * FROM `$tabla` WHERE Nombre='$dato1'";
     $resultado1 = mysqli_query($conexion, $consulta1);
-    echo $consulta1;
-    echo "<br>";
+    echo $consulta1 . "<br>";
 
     while ($fila1 = mysqli_fetch_assoc($resultado1)) {
         $iden = $fila1['ID'];
     }
 
-    echo "ID:" . $iden;
-    echo "<br>";
+    echo "ID: $iden<br>";
 
     try {
-
         $sql = "UPDATE $tabla SET `$fila5`= (`$fila4`-`$fila3`)";
         $resultado = mysqli_query($conexion, $sql);
-        echo $sql;
+        echo $sql . "<br>";
     } catch (PDOException $e) {
         $conn = null;
-        echo $e;
-        echo "<br>";
-        echo $sql;
+        echo $e . "<br>";
+        echo $sql . "<br>";
     }
-
-    echo "<br>";
 
     try {
-        $sql = "INSERT INTO $tabla7 (`$fila9`, `$fila12`,`$titulo4`) VALUES ('" .   $iden . "', '" . $dias . "', '" . $fecha_now . "');";
+        $sql = "INSERT INTO $tabla7 (`$fila9`, `$fila12`,`$titulo4`) VALUES ('$iden', '$dias', '$fecha_now')";
         $resultado = mysqli_query($conexion, $sql);
-        echo $sql;
+        echo $sql . "<br>";
     } catch (PDOException $e) {
         $conn = null;
-        echo $e;
-        echo "<br>";
-        echo $sql;
+        echo $e . "<br>";
+        echo $sql . "<br>";
     }
 
-    $sql1      = ("UPDATE $tabla SET Cantidad = ( SELECT COUNT(*) AS cantidad_productos FROM $tabla7 WHERE $tabla.ID = $tabla7.$fila9) ;");
-    echo $sql1;
+    $sql1 = "UPDATE $tabla SET Cantidad = (SELECT COUNT(*) AS cantidad_productos FROM $tabla7 WHERE $tabla.ID = $tabla7.$fila9);";
+    echo $sql1 . "<br>";
     $consulta = mysqli_query($conexion, $sql1);
-    echo "<br>";
 } else {
+    // Registro existe, realizar operaciones de actualización y mostrar error
 
-    echo "$dato1 existe en $tabla";
-    echo "<br>";
+    echo "$dato1 existe en $tabla<br>";
 
     try {
-
         $sql = "UPDATE $tabla SET `$fila5`= (`$fila4`-`$fila3`)";
         $resultado = mysqli_query($conexion, $sql);
-        echo $sql;
+        echo $sql . "<br>";
     } catch (PDOException $e) {
         $conn = null;
-        echo $e;
-        echo "<br>";
-        echo $sql;
+        echo $e . "<br>";
+        echo $sql . "<br>";
     }
 
-    echo "<br>";
-
-    $sql1      = ("UPDATE $tabla SET Cantidad = ( SELECT COUNT(*) AS cantidad_productos FROM $tabla7 WHERE $tabla.ID = $tabla7.$fila9) ;");
-    echo $sql1;
+    $sql1 = "UPDATE $tabla SET Cantidad = (SELECT COUNT(*) AS cantidad_productos FROM $tabla7 WHERE $tabla.ID = $tabla7.$fila9);";
+    echo $sql1 . "<br>";
     $consulta = mysqli_query($conexion, $sql1);
-    echo "<br>";
-
+    
     echo '<script>
-    Swal.fire({
-        icon: "error",
-        title: "Registro de ' . $dato1 . ' Existe en  ' . $titulo7 . '",
-        confirmButtonText: "OK"
-    }).then(function() {
-         window.location = "' . $link . '"; 
-    });
-    </script>';
+        Swal.fire({
+            icon: "error",
+            title: "Registro de ' . $dato1 . ' Existe en  ' . $titulo7 . '",
+            confirmButtonText: "OK"
+        }).then(() => {
+            window.location = "' . $link . '"; 
+        });
+        </script>';
 }
+
