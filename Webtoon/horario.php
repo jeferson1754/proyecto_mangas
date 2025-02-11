fa22573cf8361ee48bbbdbe6387e1b6c66f31620
Jeferson Vargas <jeferson.vargas@revesol.cl>
2025-02-11 11:46:35 -0300

    Se mejora logica de sql en horario de webtoon

--- Webtoon/horario.php
+++ Webtoon/horario.php
@@ -4,11 +4,39 @@ require 'bd.php';
 // Obtener la última temporada y año
 $num_query = "SELECT * FROM `num_horario` ORDER BY `num_horario`.`Num` DESC LIMIT 1";
 $num_result = mysqli_query($conexion, $num_query);
+$mostrar = mysqli_fetch_array($num_result);
+$temp = strtoupper($mostrar['Temporada']);
+$ano = $mostrar['Ano'];
 
-while ($mostrar = mysqli_fetch_array($num_result)) {
-    $temp = strtoupper($mostrar['Temporada']);
-    $ano = $mostrar['Ano'];
+// Días de la semana
+$dias = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'];
+
+// Array para almacenar los resultados
+$resultados = [];
+$resultados2 = [];
+
+// Consultas y resultados en un solo ciclo
+foreach ($dias as $dia) {
+    // Obtener los nombres de los webtoons y el conteo
+    $consulta = "SELECT Nombre, `Dias Emision` 
+                 FROM `webtoon` 
+                 WHERE Estado='Emision' AND `Dias Emision` LIKE '%$dia%' 
+                 ORDER BY LENGTH(Nombre) DESC";
+    $resultado = mysqli_query($conexion, $consulta);
+
+    if ($resultado) {
+        // Guardar los resultados
+        $resultados[$dia] = mysqli_fetch_all($resultado, MYSQLI_ASSOC);
+    } else {
+        echo "Error en la consulta para $dia";
+    }
 }
+
+// Consulta para obtener el total de Webtoons
+$consulta_total = "SELECT COUNT(*) AS Total_Registros FROM webtoon WHERE Estado='Emisión' AND `Dias Emision`!='Indefinido'";
+$resultado_total = mysqli_query($conexion, $consulta_total);
+$final_webtoon = mysqli_fetch_assoc($resultado_total)['Total_Registros'];
+
 ?>
 
 <!DOCTYPE html>
@@ -18,10 +46,9 @@ while ($mostrar = mysqli_fetch_array($num_result)) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../css/bootstrap.css?v=<?php echo time(); ?>">
-    <link rel=" stylesheet" href="../css/horarios.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="../css/bootstrap.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="../css/horarios.css?v=<?php echo time(); ?>">
     <title>Horario Webtoon</title>
-
 </head>
 
 <body>
@@ -32,164 +59,55 @@ while ($mostrar = mysqli_fetch_array($num_result)) {
         <div class="auto-style12" style="font-family: Verdana, Geneva, Tahoma, sans-serif; font-size: 22px; font-weight: bold;">
             <?php echo $ano . " " . $temp; ?>
         </div>
-        <div class="auto-style11"><span style="font-family: Verdana, Geneva, Tahoma, sans-serif; font-size: 62px; ">
-                <span>HORARIO DE WEBTOON</span>
+        <div class="auto-style11">
+            <span style="font-family: Verdana, Geneva, Tahoma, sans-serif; font-size: 62px;">HORARIO DE WEBTOON</span>
         </div>
     </div>
 
     <div class="main-container">
-
         <table>
+            <?php foreach ($dias as $dia) {
+                $resultadosDia = $resultados[$dia] ?? [];
 
-            <?php
+                if (!empty($resultadosDia)) {
+                    echo "<tr><td rowspan='" . count($resultadosDia) . "' class='auto-style3 $dia'>
+                            <div class='auto-style8'>$dia</div></td>";
 
-            // Días de la semana
-            $dias = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'];
-
-            // Inicializar el array para almacenar los resultados
-            $resultados = [];
-
-            // Consultas y resultados
-            foreach ($dias as $dia) {
-                $consulta = "SELECT Nombre, `Dias Emision` FROM `webtoon` WHERE Estado='Emision' AND `Dias Emision`='$dia' ORDER BY LENGTH(Nombre) DESC";
-                $resultado = mysqli_query($conexion, $consulta);
-
-                // Verificar si hay resultados
-                if ($resultado) {
-                    // Guardar los resultados en el array
-                    $resultados[$dia] = $resultado;
-                } else {
-                    // Manejar errores si es necesario
-                    echo "Error en la consulta para $dia";
-                }
-            }
-
-            // Array para almacenar los resultados
-            $resultados2 = [];
-
-            // Consultas y resultados
-            foreach ($dias as $dia) {
-                $consulta = "SELECT COUNT(`Dias Emision`), `Dias Emision` FROM webtoon WHERE `Dias Emision`='$dia' AND Estado='Emision';";
-                $resultado = mysqli_query($conexion, $consulta);
-
-                // Verificar si hay resultados
-                if ($resultado) {
-                    // Guardar los resultados en el array
-                    $resultados2[$dia] = $resultado;
-                } else {
-                    // Manejar errores si es necesario
-                    echo "Error en la consulta para $dia";
-                }
-            }
-
-            $consulta = "SELECT COUNT(*) AS Total_Registros FROM webtoon WHERE Estado='Emisión' AND `Dias Emision`!='Indefinido';";
-            $resultado = mysqli_query($conexion, $consulta);
-
-            // Verificar si hay resultados
-            if ($resultado) {
-                // Obtener el resultado directamente
-                $fila = mysqli_fetch_assoc($resultado);
-
-                // Asignar el valor a la variable
-                $final_webtoon = $fila['Total_Registros'];
-
-                // Liberar memoria del resultado
-                mysqli_free_result($resultado);
-            } else {
-                // Manejar errores si es necesario
-                echo "Error en la consulta";
-            }
-
-            foreach ($dias as $dia) {
-                $resultadosDia = mysqli_fetch_all($resultados[$dia], MYSQLI_ASSOC);
-                $resultadosDia2 = mysqli_fetch_all($resultados2[$dia], MYSQLI_ASSOC);
-
-                // Verificar si hay resultados
-                if (!empty($resultadosDia) && !empty($resultadosDia2)) {
-                    // Iterar sobre los resultados
-                    foreach ($resultadosDia2 as $mostrar2) {
-            ?>
-                        <tr>
-                            <td rowspan="<?php echo $mostrar2['COUNT(`Dias Emision`)'] ?>" claszz="auto-style3 <?php echo $dia ?>">
-                                <div class="auto-style8"><?php echo $mostrar2['Dias Emision'] ?></div>
-                            </td>
-                        <?php
-                    }
-
-                    // Iterar sobre los resultados
                     foreach ($resultadosDia as $mostrar) {
-                        ?>
-                            <td><?php echo $mostrar['Nombre'] ?></td>
-                        </tr>
-            <?php
+                        echo "<td>{$mostrar['Nombre']}</td></tr>";
                     }
-                } // Fin del día
-            }
-
-            foreach ($resultados as $resultado) {
-                mysqli_free_result($resultado);
-            }
-
-            foreach ($resultados2 as $resultado) {
-                mysqli_free_result($resultado);
-            }
-            ?>
-
+                }
+            } ?>
         </table>
     </div>
 
     <div class="contenedor-flex">
-        <?php
-
-        // Inicializar el array para almacenar los resultados
-        $resultados = [];
-
-        // Consultas y resultados
-        foreach ($dias as $dia) {
-            $consulta = "SELECT Nombre, `Dias Emision` FROM `webtoon` WHERE Estado='Emision' AND `Dias Emision` LIKE '%$dia%' ORDER BY LENGTH(Nombre) DESC";
-            $resultado = mysqli_query($conexion, $consulta);
-
-           // echo $consulta . "<br>";
-            // Verificar si hay resultados
-            if ($resultado) {
-                // Guardar los resultados en el array
-                $resultados[$dia] = $resultado;
-            } else {
-                // Manejar errores si es necesario
-                echo "Error en la consulta para $dia";
-            }
-        }
-
-        foreach ($dias as $dia) {
-            $resultadosDia = mysqli_fetch_all($resultados[$dia], MYSQLI_ASSOC);
+        <?php foreach ($dias as $dia) {
+            $resultadosDia = $resultados[$dia] ?? [];
 
             if (!empty($resultadosDia)) {
-                echo "<article class='base'>";
-                echo "<header class='$dia'>$dia</header>";
+                echo "<article class='base'><header class='$dia'>$dia</header>";
 
                 foreach ($resultadosDia as $mostrar) {
-                    echo "<div class='borde'>" . $mostrar['Nombre'] . "</div>";
+                    echo "<div class='borde'>{$mostrar['Nombre']}</div>";
                 }
 
                 echo "</article>";
             }
-        }
-        ?>
+        } ?>
     </div>
 
     <div style="text-align: center;">
-        <h2 class="auto-style14">Total Webtoon Semana : <?php echo $final_webtoon ?> </h2>
+        <h2 class="auto-style14">Total Webtoon Semana: <?php echo $final_webtoon ?> </h2>
     </div>
-    <br>
-    <br>
 
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-
     <script src="../js/popper.min.js"></script>
     <script src="../js/bootstrap.min.js"></script>
 </body>
+
+</html>
+
 <?php
 $conexion = null;
 ?>
-
-</html>
